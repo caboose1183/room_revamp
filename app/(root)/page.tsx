4ec17c0824f9282b3be29b80b52186448d27d527
caboose1183 <sidneypang@hotmail.co.uk>
2024-05-29 20:23:19 -0400

    added clerk auth, protecting every page

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,7 +1,14 @@
+import { UserButton } from "@clerk/nextjs";
 import React from "react";
 
 function Home() {
-  return <div>Home</div>;
+  return (
+    <div>
+      <p>Home</p>
+
+      <UserButton afterSignOutUrl="/" />
+    </div>
+  );
 }
 
 export default Home;
