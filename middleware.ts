e7e924ac6b507fd6d8e94a574b3027d1d1ed5d9e
caboose1183 <sidneypang@hotmail.co.uk>
2024-06-11 21:35:12 -0400

    installed older clerk version 4.0

--- middleware.ts
+++ middleware.ts
@@ -1,18 +1,9 @@
-import { clerkMiddleware, createRouteMatcher } from "@clerk/nextjs/server";
+import { authMiddleware } from "@clerk/nextjs";
 
-const isProtectedRoute = createRouteMatcher([
-  "/",
-  new RegExp("^/transformations/\\w+$"),
-]);
-
-export default clerkMiddleware((auth, req) => {
-  if (isProtectedRoute(req)) {
-    auth().protect();
-  }
-
-  publicRoutes: ["/api/webhooks/clerk"];
+export default authMiddleware({
+  publicRoutes: ["/api/webhooks/clerk"],
 });
 
 export const config = {
-  matcher: ["/((?!.*\\..*|_next).*)", "/", "/(api|trpc)(.*)"],
+  matcher: ["/((?!.+\\.[\\w]+$|_next).*)", "/", "/(api|trpc)(.*)"],
 };
