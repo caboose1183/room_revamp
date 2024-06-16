f3799db5b228b181249513d5eff4ce19fe5a147a
caboose1183 <sidneypang@hotmail.co.uk>
2024-06-16 16:20:58 -0400

    no clerk path

--- middleware.ts
+++ middleware.ts
@@ -1,7 +1,7 @@
-import { authMiddleware } from "@clerk/nextjs/server";
+import { authMiddleware } from "@clerk/nextjs";
 
 export default authMiddleware({
-  ignoredRoutes: ["/api/webhooks(.*)"],
+  ignoredRoutes: ["/api/webhooks/clerk"],
 });
 
 export const config = {
