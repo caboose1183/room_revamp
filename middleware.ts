be37852c080be587a582bd91c1a2ee2e60907db6
caboose1183 <sidneypang@hotmail.co.uk>
2024-06-16 14:02:15 -0400

    changed username convention

--- middleware.ts
+++ middleware.ts
@@ -1,9 +1,10 @@
-import { authMiddleware } from "@clerk/nextjs/server";
+import { authMiddleware } from "@clerk/nextjs";
 
+//hooks
 export default authMiddleware({
-  ignoredRoutes: ["/api/webhooks(.*)"],
+  publicRoutes: ["/api/webhooks/clerk"],
 });
 
 export const config = {
-  matcher: ["/((?!.*\\..*|_next).*)", "/", "/(api|trpc)(.*)"],
+  matcher: ["/((?!.+\\.[\\w]+$|_next).*)", "/", "/(api|trpc)(.*)"],
 };
