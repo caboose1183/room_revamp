e06a7543ee424faba01e42bc5689818fc543ee82
caboose1183 <sidneypang@hotmail.co.uk>
2024-06-16 13:35:56 -0400

    changed to publicroutes and updated env mongo

--- middleware.ts
+++ middleware.ts
@@ -2,7 +2,7 @@ import { authMiddleware } from "@clerk/nextjs";
 
 //hooks
 export default authMiddleware({
-  ignoredRoutes: ["/api/webhooks/clerk"],
+  publicRoutes: ["/api/webhooks/clerk"],
 });
 
 export const config = {
