e907713fd984629aa3b965a35a5dad7c422e4220
caboose1183 <sidneypang@hotmail.co.uk>
2024-06-16 20:14:12 -0400

    added shared header and basic form and select field

--- app/(root)/transformations/add/[type]/page.tsx
+++ app/(root)/transformations/add/[type]/page.tsx
@@ -1,9 +1,36 @@
-import React from 'react'
+import React from "react";
+import Header from "@/components/shared/Header";
+import TransformationForm from "@/components/shared/TransformationForm";
 
-function AddTransformationTypePage() {
+import { transformationTypes } from "@/constants";
+import { auth } from "@clerk/nextjs";
+import { getUserById } from "@/lib/actions/user.actions";
+
+import { redirect } from "next/navigation";
+
+async function AddTransformationTypePage({
+  params: { type },
+}: SearchParamProps) {
+  const transformation = transformationTypes[type];
+
+  const { userId } = auth();
+
+  if (!userId) redirect("/sign-in");
+
+  const user = await getUserById(userId);
+
   return (
-    <div>AddTransformationTypePage</div>
-  )
+    <>
+      <Header title={transformation.title} subtitle={transformation.subTitle} />
+
+      <TransformationForm
+        action="Add"
+        userId={user._id}
+        type={transformation.type as TransformationTypeKey}
+        creditBalance={user.creditBalance}
+      />
+    </>
+  );
 }
 
-export default AddTransformationTypePage
+export default AddTransformationTypePage;
